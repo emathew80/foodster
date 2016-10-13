dd6d37a1e3f21330e1948014da5aec63608a308e
Elson Mathew <emathew.dev@gmail.com>
2016-10-13 15:28:07 +0000

    Connect user to places and style page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+   before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @places = Place.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
   end
@@ -11,8 +13,8 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
-    redirect_to root_path
+     current_user.places.create(place_params)
+     redirect_to root_path
   end
 
   private
