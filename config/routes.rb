021c959f72d143bfb395c2203d116a05f1028ffc
Elson Mathew <emathew.dev@gmail.com>
2016-10-13 21:32:26 +0000

    Add comments functunality

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   get 'about', to: 'places#about'
-  resources :places
-
+  
+  resources :places do
+    resources :comments, only: :create
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
