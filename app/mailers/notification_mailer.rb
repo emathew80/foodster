d90ba0c7f50c0161b6703655ef812947ca130020
Elson Mathew <emathew.dev@gmail.com>
2016-10-15 22:45:02 +0000

    Adjusting the mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@foodsterapp.com"
 
-  def comment_added
-    mail(to: "emathew.dev@gmail.com", subject: "Sweet! Someone added a comment on your place!")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email, subject: "Sweet! Someone added a comment on #{@place.name}")
+
   end
     
 end
