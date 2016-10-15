3a2cc36703480ab40405e050134d4ba0dcb5f0a4
Elson Mathew <emathew.dev@gmail.com>
2016-10-15 21:22:58 +0000

    Add email functionality for testing

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@foodsterapp.com"
 
   def comment_added
-    mail(to: "emathew.dev@gmail.com", subject: "Sweet! Someone added a comment to your place!")
+    mail(to: "emathew.dev@gmail.com", subject: "Sweet! Someone added a comment on your place!")
   end
     
 end
